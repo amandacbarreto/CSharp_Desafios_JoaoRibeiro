6851569db168876dde1c76bead2a1f4ab220742c
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-06 13:08:33 -0300

    Desafio 005 - primeira versao

---
+++ Desafio005/Desafio005/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Desafio005
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_adivinhar = new System.Windows.Forms.Button();
+            this.lbl_informacao = new System.Windows.Forms.Label();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_adivinhar
+            // 
+            this.btn_adivinhar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_adivinhar.Location = new System.Drawing.Point(70, 132);
+            this.btn_adivinhar.Name = "btn_adivinhar";
+            this.btn_adivinhar.Size = new System.Drawing.Size(100, 32);
+            this.btn_adivinhar.TabIndex = 0;
+            this.btn_adivinhar.Text = "Iniciar";
+            this.btn_adivinhar.UseVisualStyleBackColor = true;
+            this.btn_adivinhar.Click += new System.EventHandler(this.btn_adivinhar_Click);
+            // 
+            // lbl_informacao
+            // 
+            this.lbl_informacao.AllowDrop = true;
+            this.lbl_informacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_informacao.Location = new System.Drawing.Point(12, 9);
+            this.lbl_informacao.Name = "lbl_informacao";
+            this.lbl_informacao.Size = new System.Drawing.Size(223, 83);
+            this.lbl_informacao.TabIndex = 1;
+            this.lbl_informacao.Text = "Adivinhe o numero!";
+            this.lbl_informacao.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_numero.Location = new System.Drawing.Point(70, 95);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 31);
+            this.txt_numero.TabIndex = 2;
+            this.txt_numero.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_numero_KeyDown);
+            this.txt_numero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_numero_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 187);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.lbl_informacao);
+            this.Controls.Add(this.btn_adivinhar);
+            this.Name = "Form1";
+            this.Text = "Qual o número?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_adivinhar;
+        private System.Windows.Forms.Label lbl_informacao;
+        private System.Windows.Forms.TextBox txt_numero;
+    }
+}
+
---
+++ Desafio005/Desafio005/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio005
+{
+    public partial class Form1 : Form
+    {
+        int numero, tentativas = 0, minimo = 0, maximo = 1000;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_numero.Enabled = false;
+        }
+
+        private void txt_numero_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return && txt_numero.Text!="")
+            {
+                tentativas++;
+                if (Int32.Parse(txt_numero.Text) != numero)
+                {
+                    if (Int32.Parse(txt_numero.Text) > numero)
+                    {
+                        lbl_informacao.Text = "O numero é mais baixo";
+                    }
+                    else
+                    {
+                        lbl_informacao.Text = "O numero é mais alto";
+                    }
+                }
+                else
+                {
+                    lbl_informacao.Text = $"Acertou! Você precisou de {tentativas} tentativas.";
+                    txt_numero.Enabled = false;
+                    btn_adivinhar.Enabled = true;
+                    btn_adivinhar.Focus();
+                }
+            }
+        }
+
+        private void btn_adivinhar_Click(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+            numero = rdn.Next(minimo,maximo);
+            tentativas = 0;
+            lbl_informacao.Text = $"Jogo iniciado! Adivinhe o número entre {minimo} e {maximo}.";
+            txt_numero.Enabled = true;
+            txt_numero.Text = "";
+            txt_numero.Focus();
+            btn_adivinhar.Enabled = false;
+        }
+
+        private void txt_numero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            
+        }
+
+    }
+}
